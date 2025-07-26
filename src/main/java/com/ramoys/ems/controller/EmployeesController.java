f32fb0036e5e5434ef7d1c0c021dea59e9799165
Keshav <keshavmishra1001@gmail.com>
2025-07-27 03:25:32 +0530

    created API's

--- src/main/java/com/ramoys/ems/controller/EmployeesController.java
+++ src/main/java/com/ramoys/ems/controller/EmployeesController.java
@@ -1,5 +1,50 @@
 package com.ramoys.ems.controller;
 
-public class EmployeesController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramoys.ems.model.Employees;
+import com.ramoys.ems.service.EmployeesService;
+
+@RestController
+@RequestMapping("/employee/api/v1")
+public class EmployeesController {
+	@Autowired
+	EmployeesService employeesService;
+	
+	@PostMapping("/add")
+	public Employees addEmployee(@RequestBody Employees e) {
+		return employeesService.addEmployee(e);
+	}
+	
+	@GetMapping("/fetchAll")
+	public List<Employees> fetchAllEmployees(){
+		return employeesService.fetchAllEmployees();
+	}
+	
+	@GetMapping("/fetch/{id}")
+	public Employees fetchEmployee(@PathVariable long id) {
+		return employeesService.fetchEmployee(id);
+	}
+	
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Employees> updateEmployee(@PathVariable long id, @RequestBody Employees updatedEmployee) {
+		return employeesService.updateEmployee(id, updatedEmployee);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
+		return employeesService.deleteEmployee(id);
+	}
 }
--- src/main/java/com/ramoys/ems/service/EmployeesService.java
+++ src/main/java/com/ramoys/ems/service/EmployeesService.java
@@ -1,5 +1,64 @@
 package com.ramoys.ems.service;
 
-public class EmployeesService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ramoys.ems.model.Employees;
+import com.ramoys.ems.repository.EmployeesRepository;
+
+@Service
+public class EmployeesService {
+	@Autowired
+	EmployeesRepository employeesRepository;
+	
+	public Employees addEmployee(Employees e) {
+		return employeesRepository.save(e);
+	}
+	
+	public List<Employees> fetchAllEmployees(){
+		return employeesRepository.findAll();
+	}
+	
+	public Employees fetchEmployee(long id) {
+		return employeesRepository.findById(id).get();
+	}
+	
+	public ResponseEntity<Employees> updateEmployee(long id, Employees updatedEmployee) {
+		Employees existingEmployee = employeesRepository.findById(id).get();
+		
+		// Manual update of each field
+        existingEmployee.setFirstName(updatedEmployee.getFirstName());
+        existingEmployee.setLastName(updatedEmployee.getLastName());
+        existingEmployee.setEmail(updatedEmployee.getEmail());
+        existingEmployee.setPhone(updatedEmployee.getPhone());
+        existingEmployee.setEmergencyContact(updatedEmployee.getEmergencyContact());
+        existingEmployee.setGender(updatedEmployee.getGender());
+        existingEmployee.setDateOfBirth(updatedEmployee.getDateOfBirth());
+        existingEmployee.setDesignation(updatedEmployee.getDesignation());
+        existingEmployee.setDepartment(updatedEmployee.getDepartment());
+        existingEmployee.setSalary(updatedEmployee.getSalary());
+        existingEmployee.setAddress(updatedEmployee.getAddress());
+        existingEmployee.setCity(updatedEmployee.getCity());
+        existingEmployee.setState(updatedEmployee.getState());
+        existingEmployee.setZipCode(updatedEmployee.getZipCode());
+        existingEmployee.setCountry(updatedEmployee.getCountry());
+        existingEmployee.setJoiningDate(updatedEmployee.getJoiningDate());
+        existingEmployee.setEmploymentType(updatedEmployee.getEmploymentType());
+        existingEmployee.setStatus(updatedEmployee.getStatus());
+        existingEmployee.setAadhaarNumber(updatedEmployee.getAadhaarNumber());
+        
+        employeesRepository.save(existingEmployee);
+        
+        return ResponseEntity.ok(existingEmployee);
+	}
+	
+	public ResponseEntity<HttpStatus> deleteEmployee(long id){
+		Employees e = employeesRepository.findById(id).get();
+		employeesRepository.delete(e);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
