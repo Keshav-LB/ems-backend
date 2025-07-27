498b5b79cbce738269ca9d5a6ec8b195c0393a21
Keshav <keshavmishra1001@gmail.com>
2025-07-28 02:06:44 +0530

    userDetails Auth impl

--- src/main/java/com/ramoys/ems/config/SecurityConfig.java
+++ src/main/java/com/ramoys/ems/config/SecurityConfig.java
@@ -1,5 +1,43 @@
 package com.ramoys.ems.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf->csrf.disable())
+			.authorizeHttpRequests(request->request
+											.requestMatchers("/register").permitAll()
+											.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+	
+	@Bean
+	AuthenticationProvider authenticationProvider(AuthenticationConfiguration authenticationConfiguration) {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+		provider.setUserDetailsService(userDetailsService);
+		
+		return provider;
+	}
+	
 }
