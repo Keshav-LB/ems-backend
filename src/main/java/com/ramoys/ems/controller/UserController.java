5d1f8e1f40a07d1cf5e754693cd5eb9723a01820
Keshav <keshavmishra1001@gmail.com>
2025-07-28 02:05:04 +0530

    secured api throug userDetailsDB

---
+++ src/main/java/com/ramoys/ems/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.ramoys.ems.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramoys.ems.model.Users;
+import com.ramoys.ems.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/register")
+	public Users registerUser(@RequestBody Users user) {
+		return userService.registerUser(user);
+	}
+	
+}
