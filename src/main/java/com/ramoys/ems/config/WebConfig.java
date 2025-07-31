6e459c7b55b204c1c2983a39a468d35652e3cf80
Keshav <keshavmishra1001@gmail.com>
2025-07-31 11:20:12 +0530

    fixed hardcoded credentials

--- src/main/java/com/ramoys/ems/config/WebConfig.java
+++ src/main/java/com/ramoys/ems/config/WebConfig.java
@@ -1,15 +1,21 @@
 package com.ramoys.ems.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 	
+	@Value("${frontend.url}")
+	String frontEndURL;
+	
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173")
+        .allowedOrigins(frontEndURL)
         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
