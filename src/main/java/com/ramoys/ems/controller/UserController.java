e4d46f6fa67cd7e07d23ab9c6cf41aba6dffae11
Keshav <keshavmishra1001@gmail.com>
2025-07-31 09:03:15 +0530

    refactor RequestMapping

--- src/main/java/com/ramoys/ems/controller/UserController.java
+++ src/main/java/com/ramoys/ems/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.ramoys.ems.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramoys.ems.model.Users;
 import com.ramoys.ems.service.UserService;
 
 @RestController
+@RequestMapping("/user/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 	
 	@Autowired
